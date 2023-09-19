6b7d5bfef977d28842c7064b7931dac899a7f435
brocoli05 <mjkimthebest@gmail.com>
2023-09-18 22:39:55 -0400

    Spotify music player clone First Release

---
+++ spotify-clone/client/src/Login.js
@@ -0,0 +1,5 @@
+import React from "react";
+//https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3000&state&scope=%20&show_dialog";
