b4ba2e28ad90e059e04dec4e43b5fb807df89b56
brocoli05 <mjkimthebest@gmail.com>
2023-09-19 23:58:06 -0400

    Update server and client for user auth

--- spotify-clone/client/src/Login.js
+++ spotify-clone/client/src/Login.js
@@ -1,5 +1,29 @@
 import React from "react";
-//https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+import { Container } from "react-bootstrap";
 
+//https://developer.spotify.com/documentation/web-api/tutorials/code-flow
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3000&state&scope=%20&show_dialog";
+  "https://accounts.spotify.com/authorize?client_id=48be1f84e7c8478e872e401fe1324730&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+//scope: replace any spaces with %20
+/*
+    streaming
+    user-read-email
+    user-read-private: user information
+    user-library-read: user favourites
+    user-library-modify: add user favourites
+    user-read-playback-state: songs users play
+    user-modify-palyback-state: add songs users play
+  */
+export default function Login() {
+  return (
+    // Container with one button
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success" href={AUTH_URL}>
+        Login With Spotify
+      </a>
+    </Container>
+  );
+}
--- spotify-clone/server/server.js
+++ spotify-clone/server/server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const spotifyWebApi = require("spotify-web-api-node");
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.post("/login", (req, res) => {
+  const code = req.body.code;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  const spotifyApi = new spotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "48be1f84e7c8478e872e401fe1324730",
+    clientSecret: "72f119c379134dd78ec9da9e91cfbc88",
+  });
+
+  // refresh token
+  spotifyApi.authorizationCodeGrant(code).then((data) => {
+    res
+      .json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      })
+      .catch((err) => {
+        res.sendStatus(400);
+      });
+  });
 });
